7dc48a950003c8b778611e5b31db20fcd94e0004
khraii <khraii@outlook.fr>
2021-05-11 11:48:14 +0200

    mise a jour

--- src/Controller/SearchAuteurController.php
+++ src/Controller/SearchAuteurController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Livre;
+use App\Form\LivreType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +12,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SearchAuteurController extends AbstractController
 {
     /**
-     * @Route("/search/auteur", name="search_auteur")
+     * @Route("/search/auteur", name="search_auteur", methods={"POST"})
      */
     public function index(Request $request): Response
     {
+        $livre = new Livre();
+        dd($request->getContent());
+        // $form = $this->createForm(LivreType::class, $livre);
+        // $form->handleRequest($request);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     dd($form->getData());
+        // }
         return $this->render('search_auteur/index.html.twig', [
             'controller_name' => 'SearchAuteurController',
         ]);
