5fcc921656fe42a8ecaef4d188a90e1201571d41
khraii <khraii@outlook.fr>
2021-05-11 10:09:42 +0200

    mise a jour

--- src/Controller/RendreLivreController.php
+++ src/Controller/RendreLivreController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Emprunt;
 use App\Repository\EmpruntRepository;
+use DateTime;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,10 +18,19 @@ class RendreLivreController extends AbstractController
     public function index(EmpruntRepository $empruntRepository): Response
     {
         $user =$this->getUser();
-        $tbrendu = $empruntRepository->findBy(['user' => $user, 'dateRendu' => Null]);
-        dd($tbrendu);
+        $tbrendu = $empruntRepository->findBy(['user' => $user, 'date_rendu' => Null]);
         return $this->render('rendre_livre/index.html.twig', [
             'emprunts' =>  $tbrendu,
         ]);
+    }
+
+    /**
+     * @Route("/rendre/livre/{id}", name="rendu", methods={"GET","POST"})
+     */
+    public function rendre(Emprunt $emprunt): Response
+    {
+        $emprunt->setDateSortie(new \DateTime('now'));
+
+        return $this->redirectToRoute('rendre_livre');
     }
 }
