baf0b3bf81c679290cea1d7a6eded1e8e56c9081
khraii <khraii@outlook.fr>
2021-05-11 14:32:02 +0200

    mise a jour

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("cvevgezrgezrg");
+    const axios = require('axios').default;
     const form = document.forms.search;
     const url = form.action;
     let key = form.auteur.value;
@@ -7,6 +7,16 @@ document.addEventListener("DOMContentLoaded", () => {
     form.auteur.addEventListener('keyup', function() {
         let key = this.value;
         console.dir(key);
+        axios.post(url, {
+                key: key,
+            })
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+
     });
     console.dir(url);
 })
