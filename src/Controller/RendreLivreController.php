1747365a10c67cd5f104a5b575f36dc285c8982f
khraii <khraii@outlook.fr>
2021-05-11 10:18:36 +0200

    mise a jour

--- src/Controller/RendreLivreController.php
+++ src/Controller/RendreLivreController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Emprunt;
 use App\Repository\EmpruntRepository;
-use DateTime;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,12 +23,12 @@ class RendreLivreController extends AbstractController
     }
 
     /**
-     * @Route("/rendre/livre/{id}", name="rendu", methods={"GET","POST"})
+     * @Route("/rendre/livre/{id}", name="rendu", methods={"GET"})
      */
     public function rendre(Emprunt $emprunt): Response
     {
-        $emprunt->setDateSortie(new \DateTime('now'));
-
+        $emprunt->setDateRendu(new \DateTime('now'));
+        $this->getDoctrine()->getManager()->flush();
         return $this->redirectToRoute('rendre_livre');
     }
 }
