379638f477935651bc14ae734a72f8629a5ce19a
khraii <khraii@outlook.fr>
2021-05-11 14:23:43 +0200

    mise a jour

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMcontentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("cvevgezrgezrg");
+    const form = document.forms.search;
+    const url = form.action;
+    let key = form.auteur.value;
 
+    form.auteur.addEventListener('keyup', function() {
+        let key = this.value;
+        console.dir(key);
+    });
+    console.dir(url);
 })
